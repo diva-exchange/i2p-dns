f9776f65e161898ac0dc5e773d5ad08bf3d0a0b5
Manuel Agner <manuelwyss@gmx.net>
2023-01-09 15:23:38 -0500

    Set regex constants

--- src-c/controllers/I2pDns.controller.ts
+++ src-c/controllers/I2pDns.controller.ts
@@ -3,11 +3,13 @@ import logging from "../config/logging";
 import util from "../util/util";
 
 const NAMESPACE = 'i2pDNS Controller';
+const DNS_PATTERN = '^[a-z0-9-_]{3,64}\.i2p$';
+const B32_PATTERN = '^[a-z0-9]{52}$';
 
 const getDns = (req:Request, res:Response, next:NextFunction) => {
     logging.info(NAMESPACE, `Get DNS called ${req.params.dns.substring(0)}`);
 
-    let regexCheck = util.check('^[a-z0-9-_]{3,64}\.i2p$', req.params.dns);
+    let regexCheck = util.check(DNS_PATTERN, req.params.dns);
 
     // Todo: Architecture middleware?
 
@@ -26,6 +28,9 @@ const getDns = (req:Request, res:Response, next:NextFunction) => {
 
 const putDns = (req:Request, res:Response, next:NextFunction) => {
     logging.info(NAMESPACE, `Put DNS called. ${req}`);
+
+    let regexCheck = util.check(DNS_PATTERN, req.params.dns);
+    let regexB32 = util.check(B32_PATTERN, req.params.b32);
 
     // Todo: Regex & params check
     // Todo: b32 string create
