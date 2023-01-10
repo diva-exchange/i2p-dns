f5769b87929ddfa3e733fc50ac0501de856ee6b1
Yannick Schaffner <schaffneryannick@hotmail.com>
2023-01-11 00:04:09 +0100

    Clean up data objects

---
+++ src-d/diva-dns/Data/ResolveDomainNameResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace diva_dns.Data
+{
+    public class ResolveDomainNameResult
+    {
+        [JsonPropertyName("value")]
+        public string B32Address { get; set; } = "";
+    }
+}
